75522719c81cf7f22da6e66d3327e16c8da848a1
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-08-07 15:59:17 -0700

    Define inner module exports in __init__.py.

---
+++ GApi4Term/__init__.py
@@ -0,0 +1,15 @@
+from core import Config
+from core import GMail
+from core import Drive
+from core import GoogleAccounts
+from core import URLShortener
+from core import Tasks
+
+__all__ = [
+        Config,
+        GMail,
+        Drive,
+        GoogleAccounts,
+        URLShortener,
+        Tasks
+]
